b6675d0cbc2e48238cb54b8340502790101526d0
FYP <joseph.elkhourydiab@lau.edu>
2023-06-15 21:54:20 +0300

    cooment

--- proj2/register.php
+++ proj2/register.php
@@ -79,7 +79,7 @@
       <!-- sidebar -->
       <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
       <script src="js/custom.js"></script>
-      <!-- javascript --> 
+      <!-- javascjknoliript --> 
       <script src="js/owl.carousel.js"></script>
       <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
       <script>
@@ -92,5 +92,5 @@
         
 
      
-</body>
+</body>   
 </html>
