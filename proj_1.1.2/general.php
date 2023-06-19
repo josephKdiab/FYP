862bbd89b70f73175e8a66eb0e048c647da31f44
FYP <joseph.elkhourydiab@lau.edu>
2023-06-19 21:03:11 +0300

    main:general,login,register pages

--- proj_1.1.2/general.php
+++ proj_1.1.2/general.php
@@ -40,9 +40,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-                <li class="nav-item">
-                <a class="nav-link" href="login.html">Sign in</a>
-                </li>
+                
                 <li class="nav-item">
                    <a class="nav-link" href="login.html">Sign in</a>
                 </li>
