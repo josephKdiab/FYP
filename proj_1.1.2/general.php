18591039b0e200d6a2315967e0826301bdaa01dd
FYP <joseph.elkhourydiab@lau.edu>
2023-06-19 19:24:08 +0300

    1

---
+++ proj_1.1.2/general.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- basic -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- mobile metas -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+<!-- site metas -->
+<title>talentTeck</title>
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta name="author" content="">	
+<!-- bootstrap css -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<!-- style css -->
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<!-- Responsive-->
+<link rel="stylesheet" href="css/responsive.css">
+<!-- fevicon -->
+<link rel="icon" href="images/fevicon.png" type="image/gif" />
+<!-- Scrollbar Custom CSS -->
+<link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+<!-- Tweaks for older IEs-->
+<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+<!-- owl stylesheets --> 
+<link rel="stylesheet" href="css/owl.carousel.min.css">
+<link rel="stylesoeet" href="css/owl.theme.default.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+
+</head>
+<body>
+	<!-- header section start-->
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="logo" href="#"><img src="images/logo.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link" href="login.html">Sign in</a>
+                </li>
+                <li class="nav-item">
+                   <a class="nav-link" href="login.html">Sign in</a>
+                </li>
+                <li class="nav-item">
+                   <a class="nav-link" href="register.html">Sign up</a>
+                </li>
+                <li class="nav-item">
+                   <a class="nav-link" href="aboutUs.html">About us</a>
+                </li>
+               
+			
+    
+    </nav>
+	<!-- header section start-->
+
+	
+	<!-- marketing section start-->
+	<div class="marketing_section layout_padding">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="job_section">
+					    <h1 class="jobs_text">Marketing jobs</h1>
+					    <p class="dummy_text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there</p>
+					    <div class="apply_bt"><a href="#">Apply now</a></div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="image_1 padding_0"><img src="images/img-1.png"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- marketing section end-->
+	<!-- Industrial section start-->
+	<div class="marketing_section layout_padding">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="image_1 padding_0"><img src="images/img-2.png"></div>
+				</div>
+				<div class="col-md-6">
+					<div class="job_section_2">
+					    <h1 class="jobs_text">Industrial jobs</h1>
+					    <p class="dummy_text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there</p>
+					    <div class="apply_bt"><a href="#">Apply now</a></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Industrial section end-->
+	<!-- Corporate section start-->
+	<div class="marketing_section layout_padding">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="job_section">
+					    <h1 class="jobs_text">Corporate jobs</h1>
+					    <p class="dummy_text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there</p>
+					    <div class="apply_bt"><a href="#">Apply now</a></div>
+					</div>
+				</div>
+				<div class="col-md-6 padding_0">
+					<div class="image_1"><img src="images/img-3.png"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Corporate section end-->
+	<!-- Government section start-->
+	<div class="marketing_section layout_padding">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-6 padding_0">
+					<div class="image_1"><img src="images/img-4.png"></div>
+				</div>
+				<div class="col-md-6">
+					<div class="job_section_2">
+					    <h1 class="jobs_text">Government jobs</h1>
+					    <p class="dummy_text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there</p>
+					    <div class="apply_bt"><a href="#">Apply now</a></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Government section end-->
+	
+	<!-- copyright section start-->
+	<div class="copyright_section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<p class="copyright_text">Copyright 2023 All Right Reserved By lebanese university</a></p>
+				</div>
+				<div class="col-md-6">
+					<p class="cookies_text">Cookies, Privacy and Terms</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- copyright section end-->
+
+
+    <!-- Javascript files-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <!-- javascript --> 
+      <script src="js/owl.carousel.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+      <script>
+      $(document).ready(function(){
+      $(".fancybox").fancybox({
+         openEffect: "none",
+         closeEffect: "none"
+         });
+         </script>
+
+	
+     
+</body>
+</html>
